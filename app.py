4b73a006825c4150f542e977eda4e7e77380b258
Adrian <adrian.lebreault@gmail.com>
2023-08-04 15:40:30 -0400

    CRUD de vehículos

--- app.py
+++ app.py
@@ -83,6 +83,81 @@ def eliminar_cliente(id):
     finally:
         cursor.close()
         connection.close()
+    
+@app.route('/vehiculos', methods=['POST'])
+def crear_vehiculo():
+    data = request.get_json()
+    cliente_id = data['cliente_id']
+    marca = data['marca']
+    modelo = data['modelo']
+    anio = data['anio']
+    placa = data['placa']
+    color = data['color']
+    
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    try:
+        cursor.execute("INSERT INTO vehiculos (cliente_id, marca, modelo, anio, placa, color) VALUES (%s, %s, %s, %s, %s, %s)", (cliente_id, marca, modelo, anio, placa, color))
+        connection.commit()
+        return jsonify({"message": "Vehículo creado correctamente."})
+    except Exception as e:
+        connection.rollback()
+        return jsonify({"error": str(e)})
+    finally:
+        cursor.close()
+        connection.close()
+
+@app.route('/vehiculos', methods=['GET'])
+def obtener_vehiculos():
+    connection = get_db_connection()
+    cursor = connection.cursor(pymysql.cursors.DictCursor)
+    try:
+        cursor.execute("SELECT * FROM vehiculos")
+        vehiculos = cursor.fetchall()
+        return jsonify(vehiculos)
+    except Exception as e:
+        return jsonify({"error": str(e)})
+    finally:
+        cursor.close()
+        connection.close()
+
+@app.route('/vehiculos/<int:id>', methods=['PUT'])
+def actualizar_vehiculo(id):
+    data = request.get_json()
+    cliente_id = data['cliente_id']
+    marca = data['marca']
+    modelo = data['modelo']
+    anio = data['anio']
+    placa = data['placa']
+    color = data['color']
+
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    try:
+        cursor.execute("UPDATE vehiculos SET cliente_id=%s, marca=%s, modelo=%s, anio=%s, placa=%s, color=%s WHERE id=%s", (cliente_id, marca, modelo, anio, placa, color, id))
+        connection.commit()
+        return jsonify({"message": "Vehículo actualizado correctamente."})
+    except Exception as e:
+        connection.rollback()
+        return jsonify({"error": str(e)})
+    finally:
+        cursor.close()
+        connection.close()
+@app.route('/vehiculos/<int:id>', methods=['DELETE'])
+def eliminar_vehiculo(id):
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    try:
+        cursor.execute("DELETE FROM vehiculos WHERE id=%s", (id,))
+        connection.commit()
+        return jsonify({"message": "Vehículo eliminado correctamente."})
+    except Exception as e:
+        connection.rollback()
+        return jsonify({"error": str(e)})
+    finally:
+        cursor.close()
+        connection.close()
+
 
 if __name__ == '__main__':
     app.run(debug=True)
